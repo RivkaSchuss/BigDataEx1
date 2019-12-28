1ce1330395f377c500b3ca504efcc61e24fec384
dantepli <dan932@gmail.com>
2019-12-28 23:10:24 +0200

    Q2 almost done, Q3 needs to checked + final calc of pageRank pending

--- Q2.crawl.py
+++ Q2.crawl.py
@@ -1,6 +1,54 @@
+from time import sleep
+
+import lxml.html
+import requests
+from pqdict import pqdict
+import logging
+
+logging.basicConfig(format='%(levelname)s: %(asctime)s: %(message)s', level=logging.DEBUG)
+LOGGER = logging.getLogger()
+
+
+def insert_url_to_priority_queue(url, priority_queue_urls):
+    if url in priority_queue_urls:
+        current_priority = priority_queue_urls.pop(url)
+        priority_queue_urls[url] = current_priority + 1
+        LOGGER.info("added url: {0} with priority {1}".format(url, current_priority + 1))
+    else:
+        priority_queue_urls[url] = 1
+        LOGGER.info("added url: {0} with priority {1}".format(url, 1))
+
+
+def crawl_with_priority(crawled_urls, priority_queue_urls, xpaths, list_of_url_tuples):
+    wiki_prefix = 'https://en.wikipedia.org'
+    while len(crawled_urls) < 100:
+        url_to_crawl = priority_queue_urls.pop()
+        LOGGER.info("crawling {0}".format(url_to_crawl))
+        crawled_urls.add(url_to_crawl)
+        res = requests.get(url_to_crawl)
+        doc = lxml.html.fromstring(res.content)
+        for xpath in xpaths:
+            for t in doc.xpath(xpath):
+                if not str(t).startswith(wiki_prefix):
+                    current_url = wiki_prefix + str(t)
+                else:
+                    current_url = str(t)
+                list_of_url_tuples.append([url_to_crawl, current_url])
+                if current_url not in crawled_urls:
+                    insert_url_to_priority_queue(current_url, priority_queue_urls)
+                LOGGER.info("extracted {0} from url: {1}".format(current_url, url_to_crawl))
+        sleep(1)
+    no_duplicates = set(map(tuple, list_of_url_tuples))
+    return list(map(list, no_duplicates))
+
+
 def crawl(url, xpaths):
-    lists = [[]]
+    list_of_url_tuples = []
+    priority_queue_urls = pqdict({url: 1}, reverse=True)  # max-heap priority-queue
+    crawled_urls = set()
+    return crawl_with_priority(crawled_urls, priority_queue_urls, xpaths, list_of_url_tuples)
 
-    return lists
 
-print ("finished")
+a = crawl('https://en.wikipedia.org/wiki/Andy_Ram',
+          ["//text()[contains(., 'against')]/following-sibling::a/@href[contains(., 'wiki')]"])
+LOGGER.info("number of tuples: {0}".format(len(a)))
--- Q3.playerPageRank.py
+++ Q3.playerPageRank.py
@@ -1,2 +1,56 @@
+import collections
+import random
+from collections import Counter
+from time import sleep
+
+import lxml.html
+import requests
+from pqdict import pqdict
+import logging
+
+
+def should_follow_link():
+    return random.random() > 0.15
+
+
+def create_url_dict_from_list(list_of_pairs):
+    result_dict = collections.defaultdict(list)
+    for url_tuple in list_of_pairs:
+        result_dict[url_tuple[0]].append(url_tuple[1])
+    return result_dict
+
+
+def calc_pagerank(visit_count_dict, steps):
+    page_rank = {}
+    for url, visits in visit_count_dict.items():
+        page_rank[url] = visits / steps
+    return page_rank
+
+
+def get_random_value_from_dict(url_dict):
+    key = random.choice(list(url_dict))
+    return url_dict[key]
+
+
 def playerPageRank(listOfPairs):
-    print("finished")
+    steps = 100000
+    url_dict = create_url_dict_from_list(listOfPairs)
+    list_of_outgoing_links = get_random_value_from_dict(url_dict)
+    visits = Counter()
+    for step in range(0, steps):
+        if len(list_of_outgoing_links) != 0:
+            if should_follow_link():
+                target = random.choice(list_of_outgoing_links)
+                visits[target] += 1
+                if target in url_dict:
+                    list_of_outgoing_links = url_dict[target]
+                else:
+                    list_of_outgoing_links = []  # sink
+            else:
+                # random link with prob 0.15
+                list_of_outgoing_links = get_random_value_from_dict(url_dict)
+        else:
+            # no outgoing links - random link
+            list_of_outgoing_links = get_random_value_from_dict(url_dict)
+    page_rank = calc_pagerank(visits, steps=steps)
+    print(page_rank)
